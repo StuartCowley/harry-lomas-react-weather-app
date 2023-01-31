ba7c618b54855d3d19b08ab12b8ffd9ec90819e9
Harry Lomas <harry.lomas1@outlook.com>
2023-01-31 15:45:16 +0000

    compiling without error following path reroute

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "../styles/App.css";
 
 function App() {
   return (
-    <div className= "App">
+    <div className="App">
       <h1>Weather App</h1>
-      </div>
+    </div>
   );
 }
 
