e13fe067496d32117d5e86377ee4f2fb7769fc95
Harry Lomas <harry.lomas1@outlook.com>
2023-02-06 15:09:21 +0000

    added forecast details component and render in app

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -3,12 +3,14 @@ import "../styles/App.css";
 import PropTypes from "prop-types";
 import LocationDetails from './LocationDetails';
 import ForecastSummaries from "./ForecastSummaries";
+import ForecastDetails from "./ForecastDetails";
 
 function App({ location, forecasts }) {
   return (
     <div className="forecast">
       <LocationDetails city={location.city} country={location.country} />
       <ForecastSummaries forecasts={forecasts} />
+      <ForecastDetails forecast={forecasts[0]} />
     </div>
   );
 }
@@ -19,9 +21,14 @@ App.propTypes = {
       date: PropTypes.number,
       description: PropTypes.string,
       icon: PropTypes.string,
+      humidity: PropTypes.number,
       temperature: PropTypes.shape({
         max: PropTypes.number,
         min: PropTypes.number,
+      }),
+      wind: PropTypes.shape({
+        speed: PropTypes.number,
+        direction: PropTypes.string,
       }),
     }),
   ).isRequired,
