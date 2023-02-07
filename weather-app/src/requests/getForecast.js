d5aec403c5a8a3b5b5f62168add453832b4475f7
Harry Lomas <harry.lomas1@outlook.com>
2023-02-07 12:05:12 +0000

    created getForecast method updated icon prop to string

--- weather-app/src/requests/getForecast.js
+++ weather-app/src/requests/getForecast.js
@@ -1,18 +1,9 @@
 import axios from "axios";
 
-const getForecast = (
-  setSelectedDate,
-  setForecasts,
-  setLocation,
-  searchText,
-) => {
-  let endpoint = "https://cmd-shift-weather-app.onrender.com/forecast";
+const getForecast = (setSelectedDate, setForecasts, setLocation) => {
+  const endpoint = "https://cmd-shift-weather-app.onrender.com/forecast";
 
-  if (searchText) {
-    endpoint += `?city=${searchText}`;
-  }
-
-  return axios.get(endpoint).then((response) => {
+  axios.get(endpoint).then((response) => {
     setSelectedDate(response.data.forecasts[0].date);
     setForecasts(response.data.forecasts);
     setLocation(response.data.location);
