d74dcb29d2487c6954700cdd9c28a6a06616c08d
Harry Lomas <harry.lomas1@outlook.com>
2023-02-07 12:27:05 +0000

    added search form and tests passing

--- weather-app/src/components/App.js
+++ weather-app/src/components/App.js
@@ -4,17 +4,23 @@ import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import getForecast from "../requests/getForecast";
 import "../styles/App.css";
+import SearchForm from "./SearchForm";
 
 function App() {
   const [forecasts, setForecasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: "" });
   const [selectedDate, setSelectedDate] = useState(0);
 
+  const [searchText, setSearchText] = useState("");
+
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate,
   );
   const handleForecastSelect = (date) => {
     setSelectedDate(date);
+  };
+  const handleCitySearch = () => {
+    getForecast(setSelectedDate, setForecasts, setLocation, searchText);
   };
 
   useEffect(() => {
@@ -24,6 +30,11 @@ function App() {
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
+      <SearchForm
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onSubmit={handleCitySearch}
+      />
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
